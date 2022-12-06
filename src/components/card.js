ce3784cb85e69e3fa39108c9182a30d130c07951
Daf <dafne.azzolina@gmail.com>
2022-12-06 14:03:39 -0300

    Finish cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,12 @@
+import { Link } from "gatsby";
 import React from "react";
+import { gridContainer, gridItem, cardList, cardButton } from "./card.module.css";
 
 const Card = () => {
   const cards = [
     {
-      title: "Incubator",
-      img: "../images/Team.png"
+      title: "Incubator/Accelerator",
+      img: "../images/Team.png",
     },
     {
       title: "Corporations",
@@ -25,14 +27,29 @@ const Card = () => {
   ];
   return (
     <>
+    <div className={gridContainer}>
       {cards.map((card) => {
         return (
-          <div key={card.title}>
-            <p>{card.title}</p>
-            <img src={card.img} alt="" />
-          </div>
+            <div className={gridItem} key={card.title}>
+              <div>
+                <h3>{card.title}</h3>
+                <img src={card.img} alt="" />
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+              sollicitudin arcu a justo vehicula, eget sagittis turpis molestie.</p>
+              <hr />
+              <h4>Popular Features</h4>
+              <ul className={cardList}>
+                <li>Lorem ipsum</li>
+                <li>Lorem ipsum</li>
+                <li>Lorem ipsum</li>
+              </ul>
+              <Link to="/" className={cardButton} >Get Started</Link>
+            </div>
+          
         );
       })}
+      </div>
     </>
   );
 };
