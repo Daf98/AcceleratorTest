8fd2ffbffa626db32e4a63ec6fd8a55c0a5cd415
Daf <dafne.azzolina@gmail.com>
2022-12-05 23:48:15 -0300

    Fix title size

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -1,13 +1,13 @@
 import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
-import {splashScreen, splashLink, firstPar, longPar} from './splash.module.css'
+import {splashScreen, splashLink, firstPar, longPar, splashHeader} from './splash.module.css'
 
 const Splash = () => {
   return (
     <div className={splashScreen}>
         <p className={firstPar}>Join and connect</p>
-        <h1>To fasten your innovation potential</h1>
+        <h1 className={splashHeader}>To fasten your<br /> innovation potential</h1>
         <p className={longPar}>AcceleratorApp innovation Community - Tools and community to<br />
          facilitate innovation between all members of our community</p>
         <Link className={splashLink} to="demo-request">Schedule a Demo</Link>
