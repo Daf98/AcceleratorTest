8ee84ca437622ee3310931dc07bbd4510692983d
Daf <dafne.azzolina@gmail.com>
2022-12-05 23:38:43 -0300

    Develop splash screen

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -1,16 +1,19 @@
 import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
+import {splashScreen, splashLink} from './splash.module.css'
 
 const Splash = () => {
   return (
-    <div>
+    <div className={splashScreen}>
         <p>Join and connect</p>
         <h1>To fasten your innovation potential</h1>
-        <p>AcceleratorApp innovation Community - Tools and community to facilitate
-         innovation between all members of our community</p>
-        <Link to="demo-request">Schedule a Demo</Link>
-        <StaticImage src='../images/Video.png' />
+        <p>AcceleratorApp innovation Community - Tools and community to<br />
+         facilitate innovation between all members of our community</p>
+        <Link className={splashLink} to="demo-request">Schedule a Demo</Link>
+        <Link to="watch-video">
+            <StaticImage src='../images/Video.png' />
+        </Link>
         <div>Cookies</div>
     </div>
   )
