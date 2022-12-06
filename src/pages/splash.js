441dae06beac1ec593d6768e29bd82be3abe63ec
Daf <dafne.azzolina@gmail.com>
2022-12-05 23:41:51 -0300

    Finish splash screen

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -1,14 +1,14 @@
 import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
-import {splashScreen, splashLink} from './splash.module.css'
+import {splashScreen, splashLink, firstPar, longPar} from './splash.module.css'
 
 const Splash = () => {
   return (
     <div className={splashScreen}>
-        <p>Join and connect</p>
+        <p className={firstPar}>Join and connect</p>
         <h1>To fasten your innovation potential</h1>
-        <p>AcceleratorApp innovation Community - Tools and community to<br />
+        <p className={longPar}>AcceleratorApp innovation Community - Tools and community to<br />
          facilitate innovation between all members of our community</p>
         <Link className={splashLink} to="demo-request">Schedule a Demo</Link>
         <Link to="watch-video">
