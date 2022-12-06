1fbf6f22343160f241b0494b89f9994168ba803c
Daf <dafne.azzolina@gmail.com>
2022-12-06 14:57:28 -0300

    Fix card rendering

--- src/components/card.js
+++ src/components/card.js
@@ -1,42 +1,56 @@
 import { Link } from "gatsby";
 import React from "react";
-import { gridContainer, gridItem, cardList, cardButton } from "./card.module.css";
+import {
+  gridContainer,
+  gridItem,
+  cardList,
+  cardButton,
+  gridContent,
+} from "./card.module.css";
+import icon1 from "../images/cards/Interactive.png";
+import icon2 from "../images/cards/Team.png";
+import icon3 from "../images/cards/Rocket.png";
+import icon4 from "../images/cards/ActualRocket.png";
+import icon5 from "../images/cards/Engage.png";
 
 const Card = () => {
   const cards = [
     {
       title: "Incubator/Accelerator",
-      img: "../images/Team.png",
+      img: icon1,
     },
     {
       title: "Corporations",
-      img: "../images/Engage.png",
+      img: icon2,
     },
     {
       title: "Investor",
-      img: "../images/Engage.png",
+      img: icon3,
     },
     {
       title: "Startup",
-      img: "../images/Engage.png",
+      img: icon4,
     },
     {
       title: "Open Challenge",
-      img: "../images/Engage.png",
+      img: icon5,
     },
   ];
   return (
     <>
-    <div className={gridContainer}>
-      {cards.map((card) => {
-        return (
+      <div className={gridContainer}>
+        {cards.map((card) => {
+          return (
             <div className={gridItem} key={card.title}>
-              <div>
-                <h3>{card.title}</h3>
+              <div className={gridContent}>
                 <img src={card.img} alt="" />
+                <h3>{card.title}</h3>
               </div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              sollicitudin arcu a justo vehicula, eget sagittis turpis molestie.</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Praesent sollicitudin arcu a justo vehicula, eget sagittis
+                turpis molestie.
+              </p>
               <hr />
               <h4>Popular Features</h4>
               <ul className={cardList}>
@@ -44,11 +58,12 @@ const Card = () => {
                 <li>Lorem ipsum</li>
                 <li>Lorem ipsum</li>
               </ul>
-              <Link to="/" className={cardButton} >Get Started</Link>
+              <Link to="/" className={cardButton}>
+                Get Started
+              </Link>
             </div>
-          
-        );
-      })}
+          );
+        })}
       </div>
     </>
   );
