ac8189615f0244470a1b922c8ed9cd8851b6eae0
Daf <dafne.azzolina@gmail.com>
2022-12-05 23:03:08 -0300

    Add font

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,18 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Poppins`,
+            file: `https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap`,
+          },
+        ],
+      },
+    },
   ],
 };
