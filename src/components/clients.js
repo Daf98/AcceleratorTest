b4682e915165f097eafc99b01f3af23a5f08f5de
Daf <dafne.azzolina@gmail.com>
2022-12-06 21:02:06 -0300

    Finish call to action

--- src/components/clients.js
+++ src/components/clients.js
@@ -2,17 +2,22 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import image from "../images/Germany.png";
-import image2 from "../images/Client.png";
 import { Link } from "gatsby";
 import {
   clientContainer,
   clientPhoto,
   clientCarousel,
   clientPhotoID,
+  callToAction,
+  clientPar,
+  demoRequest,
+  contactButton,
+  actionButtons
 } from "./clients.module.css";
 
 const Clients = () => {
   return (
+    <>
     <div className={clientContainer}>
       <div>
         <h1>What our customers say</h1>
@@ -71,15 +76,16 @@ const Clients = () => {
         </div>
       </Carousel>
     </div>
+    <div className={callToAction}>
+        <p id={clientPar}>What are you waiting for?</p>
+        <h1>Join +150 companies that already trust us for their growth</h1>
+        <div className={actionButtons}>
+        <Link to="/demo-request" id={demoRequest}>Request a Demo</Link>
+        <Link to="/contact" id={contactButton}>Contact Us</Link>
+        </div>
+    </div>
+    </>
   );
 };
 
 export default Clients;
-
-// Don't forget to include the css in your page
-
-// Using webpack or parcel with a style loader
-// import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
-
-// Using html tag:
-// <link rel="stylesheet" href="<NODE_MODULES_FOLDER>/react-responsive-carousel/lib/styles/carousel.min.css"/>
