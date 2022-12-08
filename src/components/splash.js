788835c102b90447c42ce21dab0364dd92a24568
Daf <dafne.azzolina@gmail.com>
2022-12-08 20:15:40 -0300

    Fix heading padding

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,21 +1,35 @@
-import { Link } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
-import React from 'react'
-import {splashScreen, splashLink, firstPar, longPar, splashHeader} from './splash.module.css'
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
+import {
+  splashScreen,
+  splashLink,
+  firstPar,
+  longPar,
+  splashHeader,
+} from "./splash.module.css";
 
 const Splash = () => {
   return (
     <div className={splashScreen}>
-        <p className={firstPar}>Join in and connect</p>
-        <h1 className={splashHeader}>To fasten your<br /> innovation potential</h1>
-        <p className={longPar}>AcceleratorApp innovation Community - Tools and community to<br />
-         facilitate innovation between all members of our community</p>
-        <Link className={splashLink} to="demo-request">Schedule a Demo</Link>
-        <Link to="watch-video">
-            <StaticImage src='../images/Video.png' alt="" />
-        </Link>
+      <p className={firstPar}>Join in and connect</p>
+      <h1 className={splashHeader}>
+        To fasten your
+        <br /> innovation potential
+      </h1>
+      <p className={longPar}>
+        AcceleratorApp innovation Community - Tools and community to
+        <br />
+        facilitate innovation between all members of our community
+      </p>
+      <Link className={splashLink} to="demo-request">
+        Schedule a Demo
+      </Link>
+      <Link to="watch-video">
+        <StaticImage src="../images/Video.png" alt="" />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Splash
+export default Splash;
