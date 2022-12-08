cebe8646e35e8e1cef1bea16acffd1d53b72f9a3
Daf <dafne.azzolina@gmail.com>
2022-12-08 18:32:11 -0300

    Partially style mobile navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,25 +1,76 @@
-import React from 'react'
-import { StaticImage } from "gatsby-plugin-image"
-import {navbar, navLogo, navLink, signButton, demoRequest} from 
-"./navbar.module.css"
-import { Link } from 'gatsby'
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import {
+  navbar,
+  navLogo,
+  navLink,
+  signButton,
+  demoRequest,
+  desktopNavbar,
+  mobileNavbar,
+} from "./navbar.module.css";
+import { Link } from "gatsby";
+import { slide as Menu } from "react-burger-menu";
 
 const Navbar = () => {
   return (
-    <nav className={navbar}>
-    <div className={navLogo}>
-        <StaticImage src='../images/logo.png' alt="Logo" />
-        <p>AcceleratorApp</p>
-    </div>
-    <ul>
-        <li><Link className={navLink} to="/community">Community</Link></li>
-        <li><Link className={navLink} to="/modules">Modules</Link></li>
-        <li><Link className={navLink} to="/pricing">Pricing</Link></li>
-        <li><Link className={navLink} id={signButton} to="/sign-in">Sign In</Link></li>
-        <li><Link className={navLink} id={demoRequest} to="/demo-request">Request a Demo</Link></li>
-    </ul>
-    </nav>
-  )
-}
+    <>
+      <nav className={desktopNavbar}>
+        <div className={navLogo}>
+          <StaticImage src="../images/logo.png" alt="Logo" />
+          <p>AcceleratorApp</p>
+        </div>
+        <ul>
+          <li>
+            <Link className={navLink} to="/community">
+              Community
+            </Link>
+          </li>
+          <li>
+            <Link className={navLink} to="/modules">
+              Modules
+            </Link>
+          </li>
+          <li>
+            <Link className={navLink} to="/pricing">
+              Pricing
+            </Link>
+          </li>
+          <li>
+            <Link className={navLink} id={signButton} to="/sign-in">
+              Sign In
+            </Link>
+          </li>
+          <li>
+            <Link className={navLink} id={demoRequest} to="/demo-request">
+              Request a Demo
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <nav className={mobileNavbar}>
+      <StaticImage src="../images/logo.png" alt="Logo" />
+      <p>Hamburger menu</p>
+        {/* <Menu right>
+          <Link className={navLink} to="/community">
+            Community
+          </Link>
+          <Link className={navLink} to="/modules">
+            Modules
+          </Link>
+          <Link className={navLink} to="/pricing">
+            Pricing
+          </Link>
+          <Link className={navLink} id={signButton} to="/sign-in">
+            Sign In
+          </Link>
+          <Link className={navLink} id={demoRequest} to="/demo-request">
+            Request a Demo
+          </Link>
+        </Menu> */}
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
